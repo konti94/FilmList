17612cf11cfaddbeb5f327088543201ba793bcce
konti <kontos.balazs.94@gmail.com>
2024-02-27 11:43:03 +0100

    create navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,47 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import logo from '../assets/logo.svg';
+
+function FLNav() {
+	return (
+		<Navbar collapseOnSelect expand='lg' className='bg-body-tertiary'>
+			<Container>
+				<Navbar.Brand
+					href='/'
+					className='d-flex align-items-center'>
+					<img src={logo} alt='' className='flnav-logo me-2' />
+					<h1 className='m-0 p-0 fs-3'>FilmList</h1>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+				<Navbar.Collapse id='responsive-navbar-nav'>
+					<Form
+						inline
+						className='ms-auto d-flex align-items-center flnav-searchbar'>
+						<Form.Control
+							type='text'
+							placeholder='Search'
+							className='mr-sm-2 flnav-search-input'
+						/>
+						<Button
+							type='submit'
+							className='text-white flnav-search-button'>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								width='16'
+								height='16'
+								fill='currentColor'
+								className='bi bi-search'
+								viewBox='0 0 16 16'>
+								<path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0' />
+							</svg>
+						</Button>
+					</Form>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+}
+
+export default FLNav;
