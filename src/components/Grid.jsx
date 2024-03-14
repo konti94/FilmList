3f3c4c1667e749bdfe43be4ef9a913ca56aa5c91
konti <kontos.balazs.94@gmail.com>
2024-03-14 17:01:54 +0100

    finishes grid, cards ui

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,6 +4,20 @@ import Card from 'react-bootstrap/Card';
 function FLGrid(props) {
 	const {films} = props;
 
+	const handleMouseOver = (e) => {
+		const element = e.target.nextElementSibling;
+		element?.classList?.remove('d-none');
+	}
+
+	const handleMouseLeave = (e) => {
+		const element = e.target;
+		if (element.classList.contains('rounded-2')) {
+			element?.classList?.add('d-none');
+		} else {
+			element?.parentNode.classList?.add('d-none');
+		}
+	}
+
 	return (
 		<div className='container py-4 py-lg-5'>
 			{
@@ -11,9 +25,9 @@ function FLGrid(props) {
 				?	<div className='grid'>
 						{
 							films.map(film => (
-								<Card key={film.imdbID} className='g-col-6 g-col-md-4 border-0 position-relative' style={{height: '600px'}}>
-									<Card.Img variant='top' src={film.Poster} style={{height: '600px'}} />
-									<Card.Body className='position-absolute h-100 w-100 bg-dark bg-opacity-75'>
+								<Card key={film.imdbID} className='g-col-6 g-col-md-4 border-0 position-relative' style={{height: '600px'}} onMouseEnter={handleMouseOver} onMouseLeave={handleMouseLeave}>
+									<Card.Img variant='center' src={film.Poster} className='rounded-2' style={{height: '600px'}} />
+									<Card.Body className='position-absolute h-100 w-100 rounded-2 bg-dark bg-opacity-75 d-none'>
 										<div className='position-absolute top-50 start-0 text-white text-center w-100 px-4'>
 											<Card.Title className='fs-2'>{film.Title}</Card.Title>
 											<Card.Text className='fs-3'>
